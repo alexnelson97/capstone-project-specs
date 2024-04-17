8ad9d2f0bcfa5a38defee65f88325a0d82f57ade
“your <alexnelson97@gmail.com>
2024-04-17 08:28:37 -0600

    Working carousel - prostgres integration

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,49 @@
 import react from "react";
 import "./Home.css";
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+};
 
 function Home() {
   return (
-    <div className="Home">
-      <h1>This is my Home Component</h1>
+    <div>
+      <div className="hero-carousel">
+        <Slider {...settings}>
+          <div>
+            <img src="your-image-1.jpg" alt="First slide" />
+          </div>
+          <div>
+            <img src="your-image-2.jpg" alt="Second slide" />
+          </div>
+          <div>
+            <img src="your-image-3.jpg" alt="Third slide" />
+          </div>
+        </Slider>
+      </div>
+      <div className="section">
+        <p>Text here</p>
+        <img src="path-to-your-image.jpg" alt="Description" />
+      </div>
+      <div className="section">
+        <p>Text here</p>
+        <img src="path-to-your-image.jpg" alt="Description" />
+      </div>
+      <div className="section">
+        <p>Text here</p>
+        <img src="path-to-your-image.jpg" alt="Description" />
+      </div>
     </div>
   );
 }
